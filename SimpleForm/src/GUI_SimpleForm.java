79af5c85c18800a045844659216e7271f986f5ed
algokelvin-373 <kelvinht373@gmail.com>
2022-01-12 23:46:08 +0700

    [Simple Form] Success Create Label for Title

--- SimpleForm/src/GUI_SimpleForm.java
+++ SimpleForm/src/GUI_SimpleForm.java
@@ -4,9 +4,10 @@ import java.awt.*;
 public class GUI_SimpleForm {
     private JPanel panel;
     private JLabel title1;
+    private JLabel title2;
 
     public JPanel getPanel() {
-        panel.setPreferredSize(new Dimension(300, 150));
+        panel.setPreferredSize(new Dimension(300, 75));
         return panel;
     }
 
