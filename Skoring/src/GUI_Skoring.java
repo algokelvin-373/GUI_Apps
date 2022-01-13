a5ce5605e8aacd5b41b7c0e822e90c4e058c77f7
algokelvin-373 <kelvinht373@gmail.com>
2022-01-13 11:53:36 +0700

    [Skoring] Create Design GUI Skoring

--- Skoring/src/GUI_Skoring.java
+++ Skoring/src/GUI_Skoring.java
@@ -3,6 +3,19 @@ import java.awt.*;
 
 public class GUI_Skoring {
     private JPanel panel;
+    private JPanel panelTitleTim;
+    private JLabel txtTimA;
+    private JLabel txtTimB;
+    private JPanel panelSkorTim;
+    private JLabel txtSkorTimA;
+    private JLabel txtSkorTimB;
+    private JPanel panelBtnSkorTim;
+    private JButton btnSkorTimA;
+    private JButton btnSkorTimB;
+    private JPanel panelBtnSkorAkhir;
+    private JButton btnSkorAkhir;
+    private JPanel panelBtnResetSkor;
+    private JButton btnResetSkor;
 
     public JPanel getPanel() {
         panel.setPreferredSize(new Dimension(300, 300));
