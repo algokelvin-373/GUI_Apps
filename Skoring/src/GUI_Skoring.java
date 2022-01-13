b5f7a42745e7891f27a29d91504b8c9591e678f1
algokelvin-373 <kelvinht373@gmail.com>
2022-01-13 11:17:06 +0700

    [ Skoring ] Set Size and Title GUI Form

---
+++ Skoring/src/GUI_Skoring.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class GUI_Skoring {
+    private JPanel panel;
+
+    public JPanel getPanel() {
+        panel.setPreferredSize(new Dimension(300, 300));
+        return panel;
+    }
+
+}
---
+++ Skoring/src/Main.java
@@ -0,0 +1,9 @@
+public class Main extends FrameController {
+    public static void main(String[] args) {
+        GUI_Skoring gui = new GUI_Skoring();
+        setFrameGUI(
+                gui.getPanel(),
+                "GUI AlgoKelvin: Skoring"
+        );
+    }
+}
