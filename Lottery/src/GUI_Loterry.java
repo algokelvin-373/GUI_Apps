0a3d864e69512b9a213f31b9af4cbc52abf25d7e
algokelvin-373 <kelvinht373@gmail.com>
2022-01-13 19:21:00 +0700

    [Lottery] Done Design

--- Lottery/src/GUI_Loterry.java
+++ Lottery/src/GUI_Loterry.java
@@ -3,9 +3,18 @@ import java.awt.*;
 
 public class GUI_Loterry {
     private JPanel panel;
+    private JPanel panelLottery;
+    private JPanel panelAmountLottery;
+    private JTextField txtFieldAmountLottery;
+    private JLabel txtAmountLottery;
+    private JPanel panelResultLottery;
+    private JTextField txtFieldResultLottery;
+    private JLabel txtResultLottery;
+    private JPanel panelBtnLottery;
+    private JButton btnLottery;
 
     JPanel getPanel() {
-        panel.setPreferredSize(new Dimension(300, 200));
+        panel.setPreferredSize(new Dimension(300, 150));
         return panel;
     }
 }
