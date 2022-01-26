a52b52a3f8a1b6a9965629433e1117b199abee57
algokelvin-373 <kelvinht373@gmail.com>
2022-01-26 22:58:12 +0700

    [Statistic] Create Project

---
+++ Statistic/src/GUI_Statistic.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class GUI_Statistic {
+    private JPanel panel;
+
+    JPanel getPanel() {
+        panel.setPreferredSize(new Dimension(500, 500));
+        return panel;
+    }
+}
---
+++ Statistic/src/Main.java
@@ -0,0 +1,7 @@
+public class Main extends FrameController {
+    public static void main(String[] args) {
+        GUI_Statistic gui = new GUI_Statistic();
+        String title = "GUI AlgoKelvin: Statistic";
+        setFrameGUI(gui.getPanel(), title);
+    }
+}
