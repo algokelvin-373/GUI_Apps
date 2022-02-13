ea1b7ca7fa6c0ce12cad868901d0cbd5296fa21f
algokelvin-373 <kelvinht373@gmail.com>
2022-02-13 10:30:26 +0700

    [Image GUI] Create Projects

---
+++ ImageGUI/src/ImgGui.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class ImgGui {
+    private JPanel panel;
+    private JLabel imgIcon;
+
+    JPanel getPanel() {
+        panel.setPreferredSize(new Dimension(200, 200));
+        return panel;
+    }
+
+
+}
---
+++ ImageGUI/src/Main.java
@@ -0,0 +1,6 @@
+public class Main extends FrameController {
+    public static void main(String[] args) {
+        ImgGui gui = new ImgGui();
+        setFrameGUI(gui.getPanel(), "Image GUI");
+    }
+}
